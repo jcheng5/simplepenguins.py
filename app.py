a15ac31b3b44b9cc0d5286987afcc4889f7e63b1
Joe Cheng <joe@posit.co>
2023-05-02 17:23:37 -0700

    Updated with shiny.experimental layout

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 from pathlib import Path
-from shiny import Inputs, Outputs, Session, App, reactive, render, req, ui
-import shinyswatch
+
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+
+import shiny.experimental as x
+from shiny import App, Inputs, Outputs, Session, reactive, render, req, ui
 
 sns.set_theme()
 
 df = pd.read_csv(Path(__file__).parent / "penguins.csv", na_values="NA")
+numeric_cols = df.select_dtypes(include=["float64"]).columns.tolist()
 species = df["Species"].unique().tolist()
+species.sort()
 
-app_ui = ui.page_bootstrap(
-    {"class": "p-3"},
-    shinyswatch.theme.minty(),
-    ui.tags.style("html, body { height: 100%; }"),
-    ui.div(
-        {"class": "card h-100 p-3 d-flex flex-row"},
-        ui.div(
-            {"class": "p-3", "style": "flex: 1 1 0"},
+app_ui = x.ui.page_fillable(
+    x.ui.layout_sidebar(
+        x.ui.sidebar(
             ui.input_selectize(
-                "xvar", "X variable", df.columns.tolist(), selected="Bill Length (mm)"
+                "xvar", "X variable", numeric_cols, selected="Bill Length (mm)"
             ),
             ui.input_selectize(
-                "yvar", "Y variable", df.columns.tolist(), selected="Bill Depth (mm)"
+                "yvar", "Y variable", numeric_cols, selected="Bill Depth (mm)"
             ),
             ui.input_checkbox_group(
                 "species", "Filter by species", species, selected=species
@@ -31,10 +29,8 @@ app_ui = ui.page_bootstrap(
             ui.input_switch("by_species", "Show species", value=True),
             ui.input_switch("show_margins", "Show marginal plots", value=True),
         ),
-        ui.div(
-            {"class": "p-3", "style": "flex: 3 3 0"},
-            ui.output_plot("scatter", height="100%"),
-        ),
+        x.ui.output_plot("scatter", fill=True),
+        fillable=True,
     ),
 )
 
@@ -42,10 +38,11 @@ app_ui = ui.page_bootstrap(
 def server(input: Inputs, output: Outputs, session: Session):
     @reactive.Calc
     def filtered_df() -> pd.DataFrame:
-        """Returns a Polars data frame that includes only the desired rows"""
+        """Returns a Pandas data frame that includes only the desired rows"""
 
         # This calculation "req"uires that at least one species is selected
         req(len(input.species()) > 0)
+
         # Filter the rows so we only include the desired species
         return df[df["Species"].isin(input.species())]
 
@@ -63,7 +60,8 @@ def server(input: Inputs, output: Outputs, session: Session):
             y=input.yvar(),
             hue="Species" if input.by_species() else None,
             hue_order=species,
+            legend=False,
         )
-        plt.legend(loc="lower right")
+
 
 app = App(app_ui, server)
